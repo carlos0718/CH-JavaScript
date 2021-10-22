6da8adb284f983f61d7e627d0876909abb9dc444
manumartinz <manumartinzx@gmail.com>
2021-10-22 13:17:21 -0300

    Updated

--- Clase_07_08/Clase_08/after/after.js
+++ Clase_07_08/Clase_08/after/after.js
@@ -1,6 +1,8 @@
 /* PRACTICA AFTER */
 
 const body = document.body
+const dinero = 500;
+let color = 'green';
 body.append('Hola', ' Muchos');
 const divAMeter = document.createElement('div');
 body.append(divAMeter);
@@ -10,10 +12,28 @@ divAMeter.innerText = "Hola Mundo";
 divAMeter.innerHTML = '<b>Hola mundo</b>'; // Falla de seguridad
 
 const grueso = document.createElement('b');
-grueso.innerText = ' Prueba';
+grueso.innerText = 'Usted tiene fondo suficiente';
+// 
+if (dinero < 0) {
+    color = 'red';
+} else {
+    color = 'green'
+}
+
+grueso.style.color = color
 divAMeter.append(grueso);
 
 // divAMeter.style.display = "";
 
 // Lo sobre escribe 
 /* divAMeter.textContent = "Texto content" */
+
+let padre = document.getElementById("personas");
+
+let personas = ["Manuel", "Anto", "Maxi"];
+
+for (const persona of personas) {
+    let li = document.createElement("li");
+    li.innerHTML = persona
+    padre.appendChild(li);
+};
